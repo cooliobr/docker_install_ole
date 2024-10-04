7bc421da022cc9882a4f402242e3e96e52a056b7
cooliobr <rafael@regs.com.br>
2024-10-04 16:21:29 -0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -39,6 +39,6 @@ systemctl daemon-reload \
 nvidia-ctk runtime configure --runtime=docker --set-as-default
 systemctl restart docker
 cd /usr/local/src/docker_install_ole
-docker build -t cooliobr/local:1.0 .
+docker build -t cooliobr/mux:isdbt .
 wget https://raw.githubusercontent.com/keylase/nvidia-patch/master/patch.sh && chmod 755 patch.sh && ./patch.sh
 docker-compose -f /usr/local/src/docker_install_ole/docker-compose-encoder.yml up -d
